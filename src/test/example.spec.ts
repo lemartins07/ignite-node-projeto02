c74c426ab8e116e315922792700bba4cd12ee775
Leandro Martins <leandro.martins.85@gmail.com>
2023-03-31 22:35:26 -0300

    Criando primeiro test

---
+++ src/test/example.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest'
+
+test('o usuário deve ser capaz de criar uma nova transação', () => {
+  const responseStatusCode = 400
+
+  expect(responseStatusCode).toEqual(201)
+})
