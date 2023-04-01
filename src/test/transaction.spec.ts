bb6bb4c3fea79d16352ebdd0d13b246193e65cb4
Leandro Martins <leandro.martins.85@gmail.com>
2023-04-01 00:02:47 -0300

    Testando listagem de transações

--- src/test/transaction.spec.ts
+++ src/test/transaction.spec.ts
@@ -20,4 +20,28 @@ describe('Transactions routes', () => {
 
     expect(response.statusCode).toEqual(201)
   })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await supertest(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await supertest(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
+  })
 })
