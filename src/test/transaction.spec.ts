fa5f626f151e1cc00c5afd3a359d6c1bb451c7ad
Leandro Martins <leandro.martins.85@gmail.com>
2023-03-31 23:15:39 -0300

    Categorizando testes

---
+++ src/test/transaction.spec.ts
@@ -0,0 +1,23 @@
+import { afterAll, beforeAll, expect, it, describe } from 'vitest'
+import supertest from 'supertest'
+import app from '../app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    const response = await supertest(app.server).post('/transactions').send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
